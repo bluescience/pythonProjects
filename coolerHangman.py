54be71482fa88660a071e67ec6a0cffc1553361a
Ronald Longcape <Ronald Longcape>
2016-10-10 19:22:29 -0700

    Apparently, input is obsolete, raw_imput is now the new input...

--- coolerHangman.py
+++ coolerHangman.py
@@ -7,13 +7,31 @@ Created on Thu Oct 06 12:08:05 2016
 import random
 count = 5
 words = ("DOG", "CAT", "HORSE", "PYTHON", "KNIGHT", "EMACS")
+
 compChoice = random.choice(words)
-print(compChoice)
+#print(compChoice)
 compLen = len(compChoice) 
-print("The number I am thinking of has {} letters").format(compLen)
+userLetter = ""
+finalChoice = ""
+
+print("The number I am thinking of has {} letters.").format(compLen)
 print("You {} chances to guess a letter in the word I am thinking of. After those 5 tries, you will need to guess the entire word.").format(count)
+
 while count > 0:
-    userLetter = input("You have {} tries to guess letters left. Please input a letter.").format(count)  
-    if userLetter in compChoice:
-        
+    print("You have {} tries to guess letters left.").format(count) 
+    userLetter = raw_input("Please input a letter: ")  
+    
+#    print("test")    
+    
+    if userLetter.upper() in compChoice:
+        print("The letter you have typed in IS in my word.")
+    else:
+        print("The letter you have typed in is NOT in my word.")
     count -=1
+
+finalChoice = raw_input("Please input the entire word you think I have chosen for you: ")
+
+if finalChoice.upper() == compChoice:
+    print("YOU WIN!!!")
+else:
+    print("Sadly, you lose...")
